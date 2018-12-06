26d56b1057f7305960030f3eab3213769177e94a
Max <maxime@uponatime.tech>
2018-12-06 15:55:09 +0100

    - Small fixes

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ let questions = [
     {
         enonce: "En jQuery, que cible $('.header-item > p')",
         reponses : [
-            "Les paragraphes enfants d'un élement de classe 'header-item'",
+            "Les paragraphes enfants d'un élément de classe 'header-item'",
             "Les paragraphes de classe 'header-item'",
             "Les éléments de classe 'header-item' supérieurs aux éléments 'p'",
             "Les éléments de classe 'header-item' inférieurs aux éléments 'p'"
